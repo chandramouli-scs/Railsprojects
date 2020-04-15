a043a7d0fe11c71241c9d39a071f5c34cf7526b7
chandramouli-scs <chandramouli@sykam.org>
2020-04-15 13:47:07 +0530

    reditect to tttask id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   def update
     @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
-      redirect_to root_path
+      redirect_to @task
     else
       render 'edit'
     end
